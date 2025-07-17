7b0b0f37f2ae090f842e57bd6217855801f9653c
Thamseel Ahamed <thamseelahamed135@gmail.com>
2025-07-17 06:54:24 +0000

    Add files via upload

---
+++ JaCoCo/src/test/java/coco/CalculatorTest.java
@@ -0,0 +1,17 @@
+package coco;
+import org.junit.Test;
+import jcoco.calculator;
+import static org.junit.Assert.*;
+public class CalculatorTest {
+	@Test
+	public void testAdd() {
+		calculator c =new calculator();
+		assertEquals(5, c.add(2,  3));
+	}
+	@Test
+	public void testSubtract() {
+		calculator c =new calculator();
+			assertEquals(1, c.subtract(3,  2));
+	}
+
+}
